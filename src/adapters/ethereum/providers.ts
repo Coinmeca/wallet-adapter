70857f83bec1365c7f1cea5363aee1a04afa7746
ZINV <71391363+l-Zetta-l@users.noreply.github.com>
2024-01-20 14:16:01 +0900

    Update - Wallet Logo

--- src/adapters/ethereum/providers.ts
+++ src/adapters/ethereum/providers.ts
@@ -5,7 +5,7 @@ export const providers: Providers = {
     MetaMask: <Provider>{
         name: "MetaMask",
         url: "https://metamask.io/",
-        logo: "https://coinmeca-web3/wallets/MetaMask/logo.svg",
+        logo: "https://coinmeca-web3.vercel.app/wallets/MetaMask/logo.svg",
         adapter(url?: string) {
             return new MetaMaskWalletAdapter();
         }
@@ -13,7 +13,7 @@ export const providers: Providers = {
     CoinbaseWallet: <Provider>{
         name: "Coinbase Wallet",
         url: "https://www.coinbase.com/wallet",
-        logo: "https://avatars.githubusercontent.com/u/18060234?s=280&v=4",
+        logo: "https://coinmeca-web3.vercel.app/wallets/CoinbaseWallet/logo.svg",
         adapter(url?: string) {
             return new CoinbaseWalletAdapter();
         }
