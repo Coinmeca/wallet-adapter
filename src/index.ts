1503d72a3f952daf945ab4ca5ee40d478f098736
ZINV <71391363+l-Zetta-l@users.noreply.github.com>
2024-03-04 20:39:42 +0900

    Update - Modulize

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 export * as adapters from './adapters';
-export * as base from './base/module';
 export * as chains from './chains';
 export * as providers from './providers';
 export * as states from './states';
 export * as utils from './utils';
+
+export * as evm from './core/evm/module';
+export * as svm from './core/svm/module';
