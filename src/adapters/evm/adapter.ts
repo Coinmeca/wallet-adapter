f5e3f46c44f2b45c94699ed45bd010e5ae571dc1
ZINV-crossangle <zinv@coinmeca.net>
2024-03-06 15:04:34 +0900

    Fix - adapter module bug

--- src/adapters/evm/adapter.ts
+++ src/adapters/evm/adapter.ts
@@ -8,9 +8,9 @@ import { parseChainId } from "../../utils";
 export const adapter = (config?: object) => {
 	const { name, provider, chain, mount, unmount, update, connection } = useWallet();
 
-	const connect = async (chain: number | string | Chain, name?: string, auto?: boolean): Promise<WalletStore | void> => {
+	const connect = async (chain: number | string | Chain, name: string, auto?: boolean): Promise<WalletStore | void> => {
 		name = name?.replaceAll(" ", "");
-		const wallet: WalletAdapter = (name ? providers[name]?.adapter(config) : provider);
+		const wallet = providers[name]?.adapter(config) as WalletAdapter;
 		const c: Chain | undefined = getNetworksById(parseChainId(chain));
 		try {
 			// if (!wallet.connected || !wallet.address || wallet.address?.length === 0 || (wallet.address?.length > 0 && !wallet.address[0])) throw new WalletNotReadyError();
@@ -18,33 +18,32 @@ export const adapter = (config?: object) => {
 			if (wallet.connected && wallet.address) {
 				const w: WalletStore = {
 					name: name,
-					provider: wallet.provider,
 					address: wallet.address,
 					chain: c,
 				};
-				update(w, c);
+				update({ ...w, provider: wallet.provider }, c);
 				localStorage.setItem("wallet", JSON.stringify(w));
-				wallet.on('chainChanged', chainChanged);
-				wallet.on('accountsChanged', accountsChanged);
-				wallet.on('disconnect', disconnect);
+				wallet.on("chainChanged", chainChanged);
+				wallet.on("accountsChanged", accountsChanged);
+				wallet.on("disconnect", disconnect);
 				return w;
 			} else {
 				console.error("Wallet Already Connected");
-			};
+			}
 		} catch (error: any) {
 			console.error("Wallet Connecting Error: ", error);
 		}
 	};
 
 	const disconnect = async (): Promise<boolean> => {
-		const wallet = (provider ? providers[name || JSON.parse(localStorage.getItem("wallet") || "")?.name]?.adapter(config) : provider) as WalletAdapter;
+		const wallet = (provider || providers[name || JSON.parse(localStorage.getItem("wallet") || "")?.name]?.adapter(config)) as WalletAdapter;
 
 		try {
 			if (wallet.connected || wallet.address) await wallet.disconnect();
 			localStorage.removeItem("wallet");
-			wallet.off('chainChanged', chainChanged);
-			wallet.off('accountsChanged', accountsChanged);
-			wallet.off('disconnect', disconnect);
+			wallet.off("chainChanged", chainChanged);
+			wallet.off("accountsChanged", accountsChanged);
+			wallet.off("disconnect", disconnect);
 			unmount();
 			return true;
 		} catch (error) {
@@ -69,7 +68,7 @@ export const adapter = (config?: object) => {
 		}
 	};
 
-	const chainChanged = (chainId: string) => connection(getNetworksById(parseChainId(chainId)) || { id: parseChainId(chainId) } as Chain);
+	const chainChanged = (chainId: string) => connection(getNetworksById(parseChainId(chainId)) || ({ id: parseChainId(chainId) } as Chain));
 	const accountsChanged = (accounts: string | string[]) => update({ address: (Array.isArray(accounts) ? accounts[0] : accounts) as string });
 
 	return { connect, disconnect, switchChain };
